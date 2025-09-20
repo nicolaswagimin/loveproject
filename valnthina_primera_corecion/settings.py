bf8383e40bac0b59a4cd7b7faec949a84629c26a
Nicolas Wagimin Bravo <nicolas.wagimin@campusucc.edu.co>
2025-09-19 20:21:20 -0500

    Configura base de datos para Railway

--- valnthina_primera_corecion/settings.py
+++ valnthina_primera_corecion/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,3 +11,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Para producción, usa una clave secreta desde variables de entorno
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "clave-insegura-para-dev")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # Para desarrollo local
+        conn_max_age=600
+    )
+}
